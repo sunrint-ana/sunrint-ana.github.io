89fbd34df02db76428b22bd3e91d20779b43ca69
MisileLab <misileminecord@gmail.com>
2023-12-17 19:17:25 +0900

    init
    Signed-off-by: MisileLab <misileminecord@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use dioxus::prelude::*;
+
+fn main() {
+    // launch the web app
+    dioxus_web::launch(app);
+}
+
+// create a component that renders a div with the text "Hello, world!"
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "I use nixos btw"
+        }
+    })
+}
+
