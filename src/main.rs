07d3a9a95f5dfc577b22e61949d2e6c066944e2f
5-23 <yhanbyeol6@gmail.com>
2023-12-18 17:13:58 +0900

    feat: layout element

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use dioxus::prelude::*;
+use dioxus::{prelude::*, html::{img, div, h1}};
 
 fn main() {
     // launch the web app
@@ -8,9 +8,75 @@ fn main() {
 // create a component that renders a div with the text "Hello, world!"
 fn app(cx: Scope) -> Element {
     cx.render(rsx! {
-        div {
-            "I use nixos btw"
+        div { 
+            // background_color: "red",
+            h1 {
+                "Application n Architecture"
+            }
+            p {
+                "I use nixos btw"
+            }
+            button {
+                "sus"
+            }
         }
+
+        
+        div { 
+            // background_color: "red",
+            Award{
+                t: "10".to_string(),
+                d: "222".to_string()
+            }
+        } 
     })
 }
+#[derive(PartialEq, Props, Clone)]
+struct AwardProps{
+    t: String,
+    d: String
+}
 
+
+
+fn Award(cx: Scope<AwardProps>) -> Element {
+    cx.render(rsx!(p {
+        div {
+            display: "inline-flex",
+            font_size: "20px",
+            gap: "10px",
+            align_items: "center",
+            justify_content: "center",
+            b { "{cx.props.t}" }
+            div {
+                width: "3px",
+                height: "20px",
+                background: "#000",
+            }
+            "{cx.props.d}"
+        }
+    }))
+}
+
+#[derive(PartialEq, Props, Clone)]
+struct MemberProps{
+    profile: String,
+    name: String,
+    stack: String
+}
+
+fn Member(cx: Scope<MemberProps>) -> Element {
+    cx.render(rsx!(p {
+        div {
+            div {
+                img {
+                    src: "{cx.props.profile}"
+                }
+            }
+            div {
+                h1 { "cx.props.name" }
+                h2 { "cx.props.stack" }
+            }
+        }
+    }))
+}
